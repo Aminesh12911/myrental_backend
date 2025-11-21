212377f704aa650e90ebb6d3ac5db1113e7b2f5e
Aminesh12911 <160565830+Aminesh12911@users.noreply.github.com>
2025-11-21 10:56:09 +0530

    Update mongoose.config.js

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+const url=process.env.MONGO_DB_URL
 
 const connectToMongoose = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/rentersdb");
+    await mongoose.connect(url);
     console.log("Mongoose is connected Successfully!");
   } catch (err) {
     console.log("Something went wrong with DB");
