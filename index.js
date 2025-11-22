f59d5938371c25d22553d949f5ffdacc79f5dd10
Aminesh12911 <160565830+Aminesh12911@users.noreply.github.com>
2025-11-22 11:38:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: ["https://my-rental-blue.vercel.app", "http://localhost:5173"],
+  origin: ["https://rental-tamq.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
